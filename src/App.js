f74045fa62012ee0b1bc8a70647409f885668491
Daniela Suárez <cdanisuarez@users.noreply.github.com>
2022-07-29 19:50:59 -0500

    checkbox added: test and code

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import './App.css';
 
 function App() {
   const [buttonColor, setButtonColor] = useState('red');
+  const [buttonDisabled, setButtonDisabled] = useState(false);
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
 
   return (
     <div>
-      <button style={{ backgroundColor: buttonColor }} onClick={() => setButtonColor(newButtonColor)}>
+      <button
+        style={{ backgroundColor: buttonColor }}
+        disabled={buttonDisabled}
+        onClick={() => setButtonColor(newButtonColor)}
+      >
         Change to {newButtonColor}
       </button>
+
+      <input
+        type="checkbox"
+        onClick={(e) => setButtonDisabled(e.target.checked)}
+      />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,15 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
+test('initial condition', () => {
+  render(<App />);
+  const button = screen.getByRole('button', { name: 'Change to blue' });
+  expect(button).toBeEnabled();
+
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).not.toBeChecked();
+});
+
 test('button has correct initial color', () => {
   render(<App />);
   const colorButton = screen.getByRole('button', { name: 'Change to blue' });
@@ -10,3 +19,16 @@ test('button has correct initial color', () => {
   expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
   expect(colorButton.textContent).toBe('Change to red');
 });
+
+test('checkbox is clicked and button is disabled/enabled', () => {
+  render(<App />);
+  const button = screen.getByRole('button', { name: 'Change to blue' });
+  const checkbox = screen.getByRole('checkbox');
+
+  fireEvent.click(checkbox);
+  expect(button).toBeDisabled();
+
+  fireEvent.click(checkbox);
+  expect(button).toBeEnabled();
+});
+
